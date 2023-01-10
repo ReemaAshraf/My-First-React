d5aebea0970df88099ea4b2978317a4bf9c90e23
ReemaAshraf <reema@approdite.com>
2023-01-10 10:48:30 -0500

    UI

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
   const items = props.items;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={items[0].title}
         amount={items[0].amount}
@@ -25,7 +26,7 @@ function Expenses(props) {
         amount={items[3].amount}
         date={items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
